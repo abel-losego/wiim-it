542dd94921fcde35bd943ae25b1da1daca0f1eaf
abel-losego <abel.losego@etu.univ-paris1.fr>
2021-05-18 09:44:49 +0200

    mon premier commit

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'site')]
+    public function index(): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+}
